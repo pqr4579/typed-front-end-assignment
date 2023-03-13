5920c59d77dfd9219e99dfa906b10ede98d1084e
crabee <dlwjdwls6504@gmail.com>
2023-03-13 20:10:49 +0900

    project-init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const PageContainer = styled.div({
+  display: "flex",
+  flex: 1,
+  minHeight: "100vh",
+  maxWidth: "100vw",
+});
+
+const App: React.FC = () => {
+  return <PageContainer></PageContainer>;
+};
 
 export default App;
