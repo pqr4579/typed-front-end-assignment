f098799d03451d3b5e6b29f77b5e7f49310c037b
crabee <dlwjdwls6504@gmail.com>
2023-03-16 03:51:26 +0900

    update : viewer loading

--- src/components/resourceViewer/viewer.tsx
+++ src/components/resourceViewer/viewer.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { Resource } from "../../atom/resource";
 import { TypedIconButton } from "typed-design-system";
+import { Overlay } from "../../designComponent";
 
 const ViewerContainer = styled.div({
   display: "flex",
@@ -32,8 +33,9 @@ const ViewerHeader = styled.div({
 const ViewContentWrapper = styled.div({
   display: "flex",
   flex: 1,
-  paddingTop: 50,
+  marginTop: 50,
   height: "100%",
+  position: "relative",
   overflowY: "scroll",
   overflowX: "hidden",
   alignItems: "center",
@@ -47,7 +49,7 @@ const ImageViewer = styled.img({
   maxWidth: "100%",
 });
 
-const URL = "https://typed.do/api/view?url=";
+const TYPED_PROXY_URL = "https://typed.do/api/view?url=";
 
 interface ResourceViewerProps {
   selectedResource: Resource | null;
@@ -59,6 +61,11 @@ const ResourceViewer: React.FC<ResourceViewerProps> = ({
   onClose,
 }) => {
   const [isViewerLoading, setIsViewerLoading] = useState<boolean>(false);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  useEffect(() => {
+    setIsViewerLoading(true);
+  }, [selectedResource]);
 
   return (
     <ViewerContainer>
@@ -78,22 +85,26 @@ const ResourceViewer: React.FC<ResourceViewerProps> = ({
         </ViewerHeader>
       )}
       <ViewContentWrapper>
-        {selectedResource && selectedResource.type === "url" && (
-          <UrlViwer
-            onLoad={() => {
-              setIsViewerLoading(false);
-            }}
-            onLoadStart={() => {
-              setIsViewerLoading(true);
-            }}
-            title={selectedResource?.type}
-            frameBorder={0}
-            sandbox={"allow-same-origin allow-scripts allow-popups allow-forms"}
-            src={URL + selectedResource.resource}
-            width="100%"
-            height="100%"
-          />
-        )}
+        {selectedResource && selectedResource.type === "url" ? (
+          <>
+            <Overlay isOpen={isViewerLoading} opacity={1} />
+            <UrlViwer
+              ref={iframeRef}
+              key={selectedResource.resource}
+              onLoad={(e) => {
+                setIsViewerLoading(false);
+              }}
+              title={selectedResource?.type}
+              frameBorder={0}
+              sandbox={
+                "allow-same-origin allow-scripts allow-popups allow-forms"
+              }
+              src={selectedResource.resource}
+              width="100%"
+              height="100%"
+            />
+          </>
+        ) : null}
         {selectedResource && selectedResource.type === "image" && (
           <ImageViewer alt="img-viewer" src={selectedResource?.resource} />
         )}
